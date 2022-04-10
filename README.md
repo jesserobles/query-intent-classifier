80bd915f1920f99fa43e64b869feafda2c0b9f41
jesserobles <jesse.robles@gmail.com>
2022-04-10 18:29:54 -0400

    Reworked huggingface demos.

--- README.md
+++ README.md
@@ -11,12 +11,30 @@
 - SNIPS - Personal assistant data with 7 intents. Includes entities. Need to parse similar to the ATIS format, which is in [CONLL](https://nlpforge.com/2021/07/13/data-annotation-for-named-entity-recognition-part-1/) format
 - search_trends - [Google search trends](https://storage.googleapis.com/covid19-open-data/covid19-vaccination-search-insights/top_queries/US_l1_vaccination_trending_searches.csv) for covid vaccines and some intents.
 
-
+## Train rasa model
 ```bash
 rasa train
 rasa run --enable-api
+```
 
-python
+## Interact via rasa API
+```python
 >>> import requests
 >>> r = requests.post('http://localhost:5005/model/parse', json={"text": "hi there"})
+```
+
+
+## Interact via python
+```python
+import os
+from rasa.core.agent import Agent
+from rasa.core.channels.channel import UserMessage
+
+model_path = model_path = os.path.join('rasa-models', 'models', '20220409-202441-old-falloff.tar.gz')
+
+agent = Agent()
+agent.load_model(model_path=model_path)
+
+message = UserMessage("hello")
+agent.processor._parse_message_with_graph(message)
 ```
---
+++ huggingface_token_classification_demo.py
@@ -0,0 +1,55 @@
+from datasets import load_dataset
+from transformers import AutoModelForTokenClassification, AutoTokenizer, DataCollatorForTokenClassification, Trainer, TrainingArguments
+
+def tokenize_and_align_labels(examples, tokenizer):
+    tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
+    labels = []
+    for i, label in enumerate(examples[f"ner_tags"]):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)  # Map tokens to their respective word.
+        previous_word_idx = None
+        label_ids = []
+        for word_idx in word_ids:  # Set the special tokens to -100.
+            if word_idx is None:
+                label_ids.append(-100)
+            elif word_idx != previous_word_idx:  # Only label the first token of a given word.
+                label_ids.append(label[word_idx])
+            else:
+                label_ids.append(-100)
+            previous_word_idx = word_idx
+        labels.append(label_ids)
+    tokenized_inputs["labels"] = labels
+    return tokenized_inputs
+
+wnut = load_dataset("wnut_17")
+label_list = wnut["train"].features[f"ner_tags"].feature.names
+
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+tokenized_input = tokenizer(wnut['train']["tokens"], is_split_into_words=True)
+tokens = tokenizer.convert_ids_to_tokens(tokenized_input["input_ids"])
+tokenized_wnut = wnut.map(tokenize_and_align_labels, batched=True, fn_kwargs={"tokenizer": tokenizer})
+
+data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+
+model = AutoModelForTokenClassification.from_pretrained("distilbert-base-uncased", num_labels=2)
+
+training_args = TrainingArguments(
+    output_dir="./results",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=3,
+    weight_decay=0.01,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_wnut["train"],
+    eval_dataset=tokenized_wnut["test"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+)
+
+trainer.train()
