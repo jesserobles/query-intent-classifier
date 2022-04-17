ab692f7177f00a26ebe312738516836fbecb19ca
jesserobles <jesse.robles@gmail.com>
2022-04-17 18:48:50 -0400

    Re-ran models with custom tokenizer, moved rasa results to new folder.

--- infer_rasa_datasets.py
+++ infer_rasa_datasets.py
@@ -57,39 +57,8 @@ with logging_redirect_tqdm():
             all_intents.append(intent)
 
         logger.info("Saving results")
-        with open(os.path.join("results", f"{model}.out"), "w") as file:
+        with open(os.path.join("results", "rasa", f"{model}.out"), "w") as file:
             file.write("\n".join(all_labels))
 
-        with open(os.path.join("results", f"{model}.label"), "w") as file:
+        with open(os.path.join("results", "rasa", f"{model}.label"), "w") as file:
             file.write("\n".join(all_intents))
-
-# Now run on the json dataset (benchmarking_data)
-# model = "benchmarking_data"
-# model_path = model_path = os.path.join('rasa-models', 'models', f'{model}.tar.gz')
-
-# agent = Agent()
-# logger.info(f"Loading model {model}")
-# agent.load_model(model_path=model_path)
-# logger.info("Finished loading model.")
-# data_path = os.path.join("datasets", f"{model.upper()}", "test")
-# logger.info("Loading data")
-# with open(os.path.join(data_path, "seq.in"), "r", encoding="utf-8") as file:
-#     texts = file.read().strip().split('\n')
-
-# logger.info("Running inference")
-# all_labels = []
-# all_intents = []
-# for text in tqdm(texts):
-#     message = UserMessage(text)
-#     parsed = agent.processor._parse_message_with_graph(message)
-#     labels = CoNLLParser.rasa_to_IOB(parsed)
-#     all_labels.append(' '.join(labels))
-#     intent = parsed['intent']['name']
-#     all_intents.append(intent)
-
-# logger.info("Saving results")
-# with open(os.path.join("results", f"{model}.out"), "w") as file:
-#     file.write("\n".join(all_labels))
-
-# with open(os.path.join("results", f"{model}.label"), "w") as file:
-#     file.write("\n".join(all_intents))
