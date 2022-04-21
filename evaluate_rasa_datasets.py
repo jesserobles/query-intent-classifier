74373493fa41127273bb31fc78c96c8151c3b279
jesserobles <jesse.robles@gmail.com>
2022-04-21 19:30:48 -0400

    Replacing rasa eval file.

--- evaluate_rasa_datasets.py
+++ evaluate_rasa_datasets.py
@@ -1,25 +1,39 @@
 import os
 
 from datasets import load_metric
-# from seqeval.metrics import accuracy_score
-# from seqeval.metrics import classification_report
-# from seqeval.metrics import f1_score
+from sklearn.metrics import classification_report
 
-data_path = os.path.join("datasets", "ATIS", "test")
-with open(os.path.join("results", "atis.out"), "r") as file:
+
+ner_metric = load_metric("seqeval")
+
+sets = ["ATIS", "BANKING77", "benchmarking_data", "CLINC150", "HWU64", "SNIPS"]
+NO_NER = set(['BANKING77', 'CLINC150', 'HWU65'])
+
+dataset_name = "ATIS"
+dataset_name = 'BANKING77'
+
+rasa_path = os.path.join("results", "rasa")
+data_path = os.path.join("datasets", dataset_name, "valid")
+
+if dataset_name not in NO_NER:
+    print("Running NER")
+    # First the NER data
+    
+    with open(os.path.join(rasa_path, f"{dataset_name.lower()}.out"), "r") as file:
+        y_pred = [line.strip().split() for line in file]
+
+    
+    with open(os.path.join(data_path, "seq.out"), "r") as file:
+        y_true = [line.strip().split() for line in file]
+
+
+    ner_results = ner_metric.compute(predictions=y_pred, references=y_true)
+
+# Now the classification data
+with open(os.path.join(rasa_path, f"{dataset_name.lower()}.label"), "r") as file:
     y_pred = [line.strip().split() for line in file]
 
-with open(os.path.join(data_path, "seq.out"), "r") as file:
+with open(os.path.join(data_path, "label"), "r") as file:
     y_true = [line.strip().split() for line in file]
 
-# f1_score(y_true, y_pred)
-
-metric = load_metric("seqeval")
-results = metric.compute(predictions=y_pred, references=y_true)
-print(
-{
-        "precision": results["overall_precision"],
-        "recall": results["overall_recall"],
-        "f1": results["overall_f1"],
-        "accuracy": results["overall_accuracy"],
-    })
+print(classification_report(y_true, y_pred))
